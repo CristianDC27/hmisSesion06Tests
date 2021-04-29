a3fc90e5b421ffb819bc8c422d99df1db685ab58
Cristian David Casado <cc002@inlumine.ual.es>
2021-04-29 14:05:30 +0200

    todo puesto para firefox

--- hmisSesion06Tests/test/org/ual/hmis/equipo3/S6CambioInfoFailsTest.java
+++ hmisSesion06Tests/test/org/ual/hmis/equipo3/S6CambioInfoFailsTest.java
@@ -32,7 +32,7 @@ public class S6CambioInfoFailsTest {
   @Before
   public void setUp() {
 	// Browser selector
-	   int browser= 1; // 0: firefox, 1: chrome,...
+	   int browser= 0; // 0: firefox, 1: chrome,...
 	   boolean headless = false;
 
 	   switch (browser) {
