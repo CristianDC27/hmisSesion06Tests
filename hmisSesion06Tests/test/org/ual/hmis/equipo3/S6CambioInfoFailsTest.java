0ef1233df8ac4990a9e447072c98c1880921d268
Cristian David Casado <cc002@inlumine.ual.es>
2021-04-29 14:01:21 +0200

    properties cambiadas

--- hmisSesion06Tests/test/org/ual/hmis/equipo3/S6CambioInfoFailsTest.java
+++ hmisSesion06Tests/test/org/ual/hmis/equipo3/S6CambioInfoFailsTest.java
@@ -40,7 +40,7 @@ public class S6CambioInfoFailsTest {
 		   // Firefox
 		   // Descargar geckodriver de https://github.com/mozilla/geckodriver/releases
 		   // Descomprimir el archivo geckodriver.exe en la carpeta drivers
-		   System.setProperty("webdriver.gecko.driver",  "drivers/geckodriver.exe");
+//		   System.setProperty("webdriver.gecko.driver",  "drivers/geckodriver.exe");
 		   FirefoxOptions firefoxOptions = new FirefoxOptions();
 		   if (headless) firefoxOptions.setHeadless(headless);
 		   driver = new FirefoxDriver(firefoxOptions);
@@ -51,7 +51,7 @@ public class S6CambioInfoFailsTest {
 		   // Descargar Chromedriver de https://chromedriver.chromium.org/downloads
 		   // Descomprimir el archivo chromedriver.exe en la carpeta drivers
 
-		   System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+//		   System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
 		   ChromeOptions chromeOptions = new ChromeOptions();
 		   if (headless) chromeOptions.setHeadless(headless);
 		   chromeOptions.addArguments("window-size=1920,1080");
